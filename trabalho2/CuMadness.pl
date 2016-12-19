a811d6bde3a38679978d292a35ad05a9095949be
Nuno Miguel <up201406990@fe.up.pt>
2016-12-19 21:45:20 +0000

    Função que verifica se o nº de ocorrências de um valor numa dada lista é N

--- trabalho2/CuMadness.pl
+++ trabalho2/CuMadness.pl
@@ -1,9 +1,23 @@
 :- use_module(library(clpfd)).
+:- use_module(library(lists)).
 
-cuMadness(L,N):-
-	Units is (N * N * 6),
-	length(L, Units),
-	domain(L,1,4), Red = 1, Yellow = 2, Green = 3, Blue = 4,
-	count(Red, L, #=, RedCount),
-	labeling([maximize(RedCount)],L),
-	write(RedCount).
+%Every red peg must be next to exactly one yellow peg, no more no less
+%Every yellow peg must be next to exactly one green, no more no less
+%Every green peg must be next to exactly one blue, no more no less
+%Every blue peg must be next to exactly one red, no more no less
+
+%The colours of its other neighbours do not matter
+
+cuMadness(Cube, Side):-
+	Units is (Side * Side),
+	length(Cube, Units),
+	domain(Cube, 1, 4), Red = 1, Yellow = 2, Green = 3, Blue = 4,
+	count(Red, Cube, #=, RedCount),
+	labeling([maximize(RedCount)],Cube).
+	
+%declared in sictus manual
+exactly(_, [], 0). 
+exactly(Color, [L|Ls], N):-
+	Color #= L #<=> A,
+	N #= M+A,
+	exactly(Color, Ls, M).
