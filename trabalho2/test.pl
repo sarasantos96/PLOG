668d445f2434320fbb73c03455f5871aa349c343
Sara Santos <santos.sara96@gmail.com>
2016-12-21 18:26:24 +0000

    progressos

--- trabalho2/test.pl
+++ trabalho2/test.pl
@@ -12,16 +12,40 @@ test(L,N):-
 	Len is N * N,
 	length(L,Len),
 	domain(L,1,4),
-	element(PositionRed,L,1),
-	element(PositionYellow,L,2),
-	element(PositionGreen,L,3),
-	element(PositionBlue,L,4),
-	validate_Pos(PositionRed,PositionYellow,PositionGreen,PositionBlue),
+	Limit is Len +1,
+	validate_face(L,1,Limit,N),
 	count(1,L,#=,SumRed),
 	labeling([maximize(SumRed)],L).
+	
+validate_face(_, Len,Len,_).
+validate_face(List,Index,Length,N):-
+	get_neighbours(Index,[T,B,L,R],N,List),
+	element(Index, List, P),
+	validate([T,B,L,R],P),
+	NewIndex is Index + 1,
+	validate_face(List,NewIndex,Length,N).
+	
 
-validate_Pos(PosRed,PosYellow,PosGreen,PosBlue):-
-	(PosRed + 1 #= PosYellow #\/ PosRed-1 #= PosYellow) #/\
-	(PosYellow +1 #= PosGreen #\/ PosYellow -1 #= PosGreen) #/\
-	(PosGreen +1 #= PosBlue #\/ PosGreen -1 #= PosBlue) #/\
-	(PosBlue +1 #= PosRed #\/ PosBlue -1 #= PosRed).
+get_neighbours(Pos,[T,B,Le,R],N,L):-
+	Top is Pos - N,
+	Bot is Pos + N,
+	Left is Pos -1,
+	Right is Pos + 1,
+	(Top > 0 -> element(Top,L,T) ; T is -1),
+	(Bot =< (N * N) -> element(Bot,L,B) ; B is -1),
+	((Pos \== 1 ,X is Left mod N, X \== 0)-> element(Left,L,Le) ; Le is -1),
+	(( Y is Pos mod N, Y \== 0 ) -> element(Right, L,R) ; R is -1).
+	
+validate([T,B,L,R],P):-
+	count(1,[T,B,L,R],#=,RedNeighbours),
+	count(2,[T,B,L,R],#=,YellowNeighbours),
+	count(3,[T,B,L,R],#=,GreenNeighbours),
+	count(4,[T,B,L,R],#=,BlueNeighbours),
+	(P #= 1 #=> YellowNeighbours #= 1);
+	(P #= 2 #=> GreenNeighbours #= 1);
+	(P #= 3 #=> BlueNeighbours #= 1);
+	(P #= 4 #=> RedNeighbours #= 1).
+
+
+
+	
