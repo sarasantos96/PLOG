e71a592d4160322e41e96b9fd400848d50e941d4
Nuno Miguel <up201406990@fe.up.pt>
2016-12-21 21:39:12 +0000

    restrições para face já funcionam

--- trabalho2/test.pl
+++ trabalho2/test.pl
@@ -13,7 +13,7 @@ test(L,N):-
 	length(L,Len),
 	domain(L,1,4),
 	Limit is Len +1,
-	validate_face(L,1,Limit,N),
+	validate_face(L,1,Len,N),
 	count(1,L,#=,SumRed),
 	labeling([maximize(SumRed)],L).
 	
@@ -25,7 +25,6 @@ validate_face(List,Index,Length,N):-
 	NewIndex is Index + 1,
 	validate_face(List,NewIndex,Length,N).
 	
-
 get_neighbours(Pos,[T,B,Le,R],N,L):-
 	Top is Pos - N,
 	Bot is Pos + N,
@@ -34,16 +33,16 @@ get_neighbours(Pos,[T,B,Le,R],N,L):-
 	(Top > 0 -> element(Top,L,T) ; T is -1),
 	(Bot =< (N * N) -> element(Bot,L,B) ; B is -1),
 	((Pos \== 1 ,X is Left mod N, X \== 0)-> element(Left,L,Le) ; Le is -1),
-	(( Y is Pos mod N, Y \== 0 ) -> element(Right, L,R) ; R is -1).
+	((Y is Pos mod N, Y \== 0) -> element(Right,L,R) ; R is -1).
 	
 validate([T,B,L,R],P):-
 	count(1,[T,B,L,R],#=,RedNeighbours),
 	count(2,[T,B,L,R],#=,YellowNeighbours),
 	count(3,[T,B,L,R],#=,GreenNeighbours),
 	count(4,[T,B,L,R],#=,BlueNeighbours),
-	(P #= 1 #=> YellowNeighbours #= 1);
-	(P #= 2 #=> GreenNeighbours #= 1);
-	(P #= 3 #=> BlueNeighbours #= 1);
+	(P #= 1 #=> YellowNeighbours #= 1),
+	(P #= 2 #=> GreenNeighbours #= 1),
+	(P #= 3 #=> BlueNeighbours #= 1),
 	(P #= 4 #=> RedNeighbours #= 1).
 
 
